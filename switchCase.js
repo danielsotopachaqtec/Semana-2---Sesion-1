1e49d214cde9091f682a057b5ba6bd8234db125b
Giovanni Morais <giovanni@lacafetalab.pe>
2020-03-09 19:55:40 -0500

    [feature]- switch case statements without comments

--- switchCase.js
+++ switchCase.js
@@ -1,73 +1,73 @@
-// let value = 500;
+let value = 500;
 
-// switch(value) {
-//     case 200:
-//     // execute case x code block
-//     console.log('Su operacion ha sido exitosa!');
-//     break;
-//     case 400:
-//     // excute case x code block
-//     console.log('Su operacion no pudo realizar en este momento, contacte al administrador de sistemas!')
-//     break;
-//     default:
-//     // execute default code block
-//     console.log('Servidor no disponible');
-// }
+switch(value) {
+    case 200:
+    // execute case x code block
+    console.log('Su operacion ha sido exitosa!');
+    break;
+    case 400:
+    // excute case x code block
+    console.log('Su operacion no pudo realizar en este momento, contacte al administrador de sistemas!')
+    break;
+    default:
+    // execute default code block
+    console.log('Servidor no disponible');
+}
 
 
-// const day = new Date().getDay();
-// console.log('day', day)
+const day = new Date().getDay();
+console.log('day', day)
 
-// switch(day) {
-//     case 1:
-//         console.log('Happy Monday');
-//         break;
-//     case 2:
-//         console.log("It's Tusday. You got this!");
-//         break;
-//     case 3:
-//         console.log('Hump day already');
-//         break;
-//     case 4:
-//         console.log("Just one more day 'til the weekend!");
-//         break;
-//     case 5:
-//         console.log('Happy Friday!');
-//         break;
-//     case 6:
-//         console.log("Have a wonderfull Saturday!");
-// }
+switch(day) {
+    case 1:
+        console.log('Happy Monday');
+        break;
+    case 2:
+        console.log("It's Tusday. You got this!");
+        break;
+    case 3:
+        console.log('Hump day already');
+        break;
+    case 4:
+        console.log("Just one more day 'til the weekend!");
+        break;
+    case 5:
+        console.log('Happy Friday!');
+        break;
+    case 6:
+        console.log("Have a wonderfull Saturday!");
+}
 
-// const month = new Date().getMonth();
+const month = new Date().getMonth();
 
-// switch(month){
-//     // January,February, March
-//     case 0:
-//     case 1:
-//     case 2:
-//         console.log('Winter');
-//         break;
-//     // April, May, June
-//     case 3:
-//     case 4:
-//     case 5:
-//         console.log('Spring');
-//         break;
-//     // July, August, September
-//     case 6:
-//     case 7:
-//     case 8:
-//         console.log('Summer');
-//         break;
-//     // October, November, December
-//     case 9:
-//     case 10:
-//     case 11:
-//         console.log('Autumn');
-//         break;
-//     default:
-//         console.log('Something went wrong!');
-// }
+switch(month){
+    // January,February, March
+    case 0:
+    case 1:
+    case 2:
+        console.log('Winter');
+        break;
+    // April, May, June
+    case 3:
+    case 4:
+    case 5:
+        console.log('Spring');
+        break;
+    // July, August, September
+    case 6:
+    case 7:
+    case 8:
+        console.log('Summer');
+        break;
+    // October, November, December
+    case 9:
+    case 10:
+    case 11:
+        console.log('Autumn');
+        break;
+    default:
+        console.log('Something went wrong!');
+}
 
 // Set the student grade
 const grade = 90;
